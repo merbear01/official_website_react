f8b84c1f769c817558ea49390817cf5cd174658c
Mercy <mercy.nwabueze-nwoji@medtronic.com>
2025-01-08 18:16:25 +0100

    my website

---
+++ learning_R/src/App.jsx
@@ -0,0 +1,14 @@
+import Cards from './Cards.jsx'
+
+function App() {
+
+  return (
+    <>
+
+    <Cards></Cards>
+      
+    </>
+  )
+}
+
+export default App;
---
+++ learning_R/src/Cards.jsx
@@ -0,0 +1,33 @@
+function Cards(){
+    return(
+
+        <>
+        <div className="about-me-container">
+            <a className="cell about-me" href="£">About Me</a>
+            <a className="cell my-project" href="£">My Projects</a>
+            </div>
+        
+
+
+        <h2 className="header-title"> Things I should have known in My 20&apos;s as a software engineer/tester</h2>
+
+        <div className="image">
+        <img className="img-container" src='https://images.squarespace-cdn.com/content/v1/5eb48d3fef49df153d320521/b9608493-9aba-48e2-a555-2acf39744910/CleopatraToyFace.jpg' alt='me'></img>
+        </div>
+
+        <div className="subheader">
+            
+            <a href="#" className="row" alt> Why Software Engineering in the first place</a>
+            <a href="#" className="row">Always ask questions</a>
+            <a href="#" className="row">Don&apos;t be afraid to break things</a>
+            <a href="#" className="row">The importance of software testing</a>
+            <a href="#" className="row">Where do you see yourself in the next 5 years</a>
+            
+        </div>
+
+        </>
+
+    );
+}
+
+export default Cards;
